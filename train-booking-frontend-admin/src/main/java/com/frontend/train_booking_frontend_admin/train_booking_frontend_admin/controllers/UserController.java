5fc9c80c6f39b64c91252d8422714ef9ef4a04ee
conganh12345 <anhvca1234@gmail.com>
2024-10-02 17:23:10 +0700

    feat: [feature/manage-user] create-user

--- train-booking-frontend-admin/src/main/java/com/frontend/train_booking_frontend_admin/train_booking_frontend_admin/controllers/UserController.java
+++ train-booking-frontend-admin/src/main/java/com/frontend/train_booking_frontend_admin/train_booking_frontend_admin/controllers/UserController.java
@@ -5,7 +5,10 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import com.frontend.train_booking_frontend_admin.train_booking_frontend_admin.models.User;
 import com.frontend.train_booking_frontend_admin.train_booking_frontend_admin.services.UserService;
@@ -19,10 +22,28 @@ public class UserController {
 	@GetMapping("/index")
 	public String index(Model model) {
 		List<User> users = userService.getAllUsers();
-		
-		model.addAttribute("page", "user")
-			.addAttribute("users", users);
+
+		model.addAttribute("page", "user").addAttribute("users", users);
 
 		return "user/index";
 	}
+
+	@GetMapping("/create")
+	public String create(Model model) {
+		model.addAttribute("page", "user");
+
+		return "user/create";
+	}
+	
+	@PostMapping("/create")
+    public String create(@ModelAttribute() User user, BindingResult result, Model model) {
+		 if (result.hasErrors()) {
+		        model.addAttribute("page", "user");  
+		        return "user/create";
+		    }
+
+        userService.addUser(user); 
+
+        return "redirect:/user/index"; 
+    }
 }
